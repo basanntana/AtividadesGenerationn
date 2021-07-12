e751cd6fd782511573c2e8e993808fdb416b968d
basanntana <barbarasantanabr@gmail.com>
2021-07-11 22:47:38 -0300

    ExercMatriz

---
+++ ExerciciosJava/ExercMatriz/Exerci03Matriz.java
@@ -0,0 +1,27 @@
+package generation.matrizes;
+
+import java.util.Scanner;
+
+public class Exerci03Matriz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner leia = new Scanner(System.in);
+		
+		int [][] duasmatri = new int [4][6];
+		
+		int l, c;
+		
+		for(l=0; l<4; l++ ) {
+			for(c=0; c<6; c++) {
+				System.out.println("Escreva o número 1: " + duasmatri[4][6] );
+		
+			}
+	  
+		     
+		}
+
+   }
+
+}
