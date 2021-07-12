086de6a9d5dfd103a2c2bc4b43e603997b24a79e
basanntana <barbarasantanabr@gmail.com>
2021-07-11 23:15:11 -0300

    ExercOrient

---
+++ ExerciciosJava/ExercOrient/ExecCliente.java
@@ -0,0 +1,25 @@
+package generationobjet;
+
+
+
+public class ExecCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cliente clienteinfo = new Cliente();
+		
+		clienteinfo.setNome("Bárbara Santana Braz");
+		clienteinfo.setTelefone("(11)959911742");
+		 
+		System.out.println("O nome da cliente é: " + clienteinfo.getNome());
+		System.out.println("O nome telefone da cliente é: " + clienteinfo.getTelefone());
+			
+			
+		
+
+	}
+	
+}	
+
+
