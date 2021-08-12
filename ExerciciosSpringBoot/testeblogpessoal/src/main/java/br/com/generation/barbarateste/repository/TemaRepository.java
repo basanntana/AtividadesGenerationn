1ee113a06d6c079aa75c3b48631f19540ee071d8
basanntana <barbarasantanabr@gmail.com>
2021-08-12 18:42:56 -0300

    Exercício SpringBoot security

--- ExerciciosSpringBoot/testeblogpessoal/src/main/java/br/com/generation/barbarateste/repository/TemaRepository.java
+++ ExerciciosSpringBoot/testeblogpessoal/src/main/java/br/com/generation/barbarateste/repository/TemaRepository.java
@@ -1,5 +1,16 @@
 package br.com.generation.barbarateste.repository;
 
-public interface TemaRepository {
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.generation.barbarateste.model.tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<tema, Long> {
+	
+	public List<tema> findAllByDescricaoContainingIgnoreCase(String descricao);
 
 }
--- ExerciciosSpringBoot/testeblogpessoal/src/main/java/br/com/generation/barbarateste/repository/UsuarioRepository.java
+++ ExerciciosSpringBoot/testeblogpessoal/src/main/java/br/com/generation/barbarateste/repository/UsuarioRepository.java
@@ -1,5 +1,23 @@
 package br.com.generation.barbarateste.repository;
 
-public interface UsuarioRepository {
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.generation.barbarateste.model.usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<usuario, Long>  {
+	
+	public Optional<usuario> findByUsuario(String usuario);
+
+	public List<usuario> findAllByNomeContainingIgnoreCase(String nome);
+
+	public usuario findByNome(String nome);
+	
+	
 
 }
